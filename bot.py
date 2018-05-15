08ad45a926945678d10af078bd896901b49273ad
CubicPill <cubicpill@gmail.com>
2018-05-15 22:55:05 +0800

    update new bus schedule

--- bot.py
+++ bot.py
@@ -18,7 +18,6 @@ def next_bus(bot: Bot, update: Update, args: list):
     text = list()
     for group_id, (int_t, line_id) in sorted(result.items(), key=lambda x: x[0]):
         text.append(sched.get_group_def(group_id) + ':')
-
         if int_t == QueryStatus.MISS_LAST:
             text.append(sched.get_line(line_id).name)
             r_str = '末班车已开出'
@@ -26,7 +25,6 @@ def next_bus(bot: Bot, update: Update, args: list):
             r_str = '今日不运行'
         else:
             text.append(sched.get_line(line_id).name)
-
             r_str = BusLine.time_to_string(int_t)
         text.append(r_str)
     update.message.reply_text('\n'.join(text))
@@ -45,7 +43,7 @@ def detail(bot: Bot, update: Update, args: list):
     if line:
         update.message.reply_text(line.to_string())
     else:
-        update.message.reply_text('线路 "{}" 不存在!')
+        update.message.reply_text('线路 "{}" 不存在!'.format(line))
 
 
 def show_help(bot, update):
